84b89469b96b51eab140a94153065ed6a04082d5
vishal siram <48790190+vishalsiram@users.noreply.github.com>
2020-06-19 18:58:56 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"vishalsiram50@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
